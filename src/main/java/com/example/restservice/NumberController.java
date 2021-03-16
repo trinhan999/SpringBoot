6c3cf9712b31f99c615f7cec659d346e5dc86b15
Nhan Phung Tri <ptrinhan@tma.com.vn>
2021-03-16 10:28:26 +0700

    logging

--- src/main/java/com/example/restservice/NumberController.java
+++ src/main/java/com/example/restservice/NumberController.java
@@ -1,15 +1,20 @@
 package com.example.restservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NumberController {
+  Logger logger = LoggerFactory.getLogger(LoggingController.class);
 
   @GetMapping("/number")
   public Number greeting(@RequestParam(value = "integer", defaultValue = "0") int n,
                          @RequestParam(value = "float", defaultValue = "1.5f") float f) {
+    logger.debug("number page");
     return new Number(n,f);
+
   }
 }
