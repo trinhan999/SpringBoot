81b4487cb0fd16615b0fed52a8fdb2145c6276cd
Nhan Phung Tri <ptrinhan@tma.com.vn>
2021-03-22 18:04:45 +0700

    spring boot

---
+++ src/main/java/com/example/demo/Number.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+public class Number {
+  private final int n;
+  private final float f;
+
+  public Number(int n, float f) {
+    this.n = n;
+    this.f = f;
+  }
+
+
+  public int getN() {
+    return n;
+  }
+
+  public float getF() {
+    return f;
+  }
+}
